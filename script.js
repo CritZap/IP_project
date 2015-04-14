0283b3eb471d8dfbdc6981871b36d73ee43ac317
Alexander Monov <alekmonov@abv.bg>
2015-04-14 06:29:04 +0300

    Update site function

--- script.js
+++ script.js
@@ -2,13 +2,52 @@ $(document).ready(function() {
     "use strict"
 
 
+    $('#textbox').hide();
+    $('#profile').hide();
+    $('#logout').hide();
+
+
+    $("#register").click(function(){
+    	var name = $("#regName").val();
+    	var pass = $("#regPass").val();
+        if (name.length<=0 || pass.length<=0) {
+            alert("You forgot to add a username or a password !");
+            return false;
+        }
+    });
+
+
+    $("#login").click(function(){
+    	var name = $("#name").val();
+    	var pass = $("#password").val();
+        if (name.length<=0 || pass.length<=0) {
+            alert("Did you forget your username or password ?");
+            return false;
+        }
+        $('#signIn').hide();
+        $('#registration').hide();
+        $('#textbox').show();
+        $('#profile').html(name + "<br>" + "@" + name.replace(' ', '')).show();
+        $('#logout').show();
+    });
+
+    $("#logout").click(function(){
+    	$('#textbox').hide();
+    	$('#profile').hide();
+    	$('#logout').hide();
+    	$('#signIn').show();
+        $('#registration').show();
+    });
+
+
     $("#post").click(function(){
         var text = $("#text").val();
+        var name = $("#name").val();
         if (text.length<=0) {
             alert("Write something !");
             return false;
         }
-        $("#tweet").html(text + "\n");
+        $("#tweet").html(name.replace(' ', '') + "<br>" + text);
     });
 
 
